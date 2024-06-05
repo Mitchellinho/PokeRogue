8dfc4edbaa4af8cb9bf3fb2dae4fcca8e259b74f
Mitchellinho <mi.gense1911@gmail.com>
2024-06-05 03:01:38 +0200

    Changed file structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { appConfig } from './app/frontend/app.config';
 import { MainMenuComponent } from './app/frontend/main-menu/main-menu.component';
 
 bootstrapApplication(MainMenuComponent, appConfig)
