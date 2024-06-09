0612eef270f2db33220558b4143e7a87a99ee3c8
Mitchellinho <mi.gense@gmx.de>
2024-06-09 03:16:29 +0200

    changed structure

--- frontend/src/main.server.ts
+++ frontend/src/main.server.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { MainMenuComponent } from './app/frontend/main-menu/main-menu.component';
-import { config } from './app/frontend/app.config.server';
+import { MainMenuComponent } from './app/main-menu/main-menu.component';
+import { config } from './app/app.config.server';
 
 const bootstrap = () => bootstrapApplication(MainMenuComponent, config);
 
